f1077a08775ba9595ee074cd1a24fef41aa5d06d
Jimmy <39@.com>
2024-03-31 18:00:06 -0400

    Add experiment

--- run_experiments_prec.sh
+++ run_experiments_prec.sh
@@ -1,3 +1,4 @@
 python main.py --dataset="mnist" --embed_dim=64 --n_attention_heads=4 --n_layers=6 --model_path="models/embed64_layers6" --wandb_run_name="embed64_layers6_bfloat16" --precision="bfloat16"
 python main.py --dataset="mnist" --embed_dim=64 --n_attention_heads=4 --n_layers=6 --model_path="models/embed64_layers6" --wandb_run_name="embed64_layers6_bfloat16_sr" --precision="bfloat16_sr"
 python main.py --dataset="mnist" --embed_dim=64 --n_attention_heads=4 --n_layers=6 --model_path="models/embed64_layers6" --wandb_run_name="embed64_layers6_bfloat16_sr" --precision="bfloat16_ac"
+python main.py --dataset="mnist" --embed_dim=64 --n_attention_heads=4 --n_layers=6 --model_path="models/embed64_layers6" --wandb_run_name="embed64_layers6_bfloat16_sr" --precision="bfloat16_alts"
